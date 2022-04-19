e8f0778b80c11faf2996d6e74b2fce5bf92009ad
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-19 12:39:33 -0300

    Add task 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,45 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.enableButton = this.enableButton.bind(this);
+
+    this.state = {
+      BandNameSearch: '',
+      IsButtonDisabled: true,
+    };
+  }
+
+  enableButton({ target }) {
+    const minTextCaracter = 2;
+    const { value } = target;
+    this.setState({
+      BandNameSearch: value,
+      IsButtonDisabled: (value.length < minTextCaracter),
+    });
+  }
+
   render() {
+    const { BandNameSearch, IsButtonDisabled } = this.state;
     return (
       <>
-        <div data-testid="page-search">Search</div>
         <Header />
+        <div data-testid="page-search">
+          <input
+            type="text"
+            value={ BandNameSearch }
+            data-testid="search-artist-input"
+            onChange={ this.enableButton }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ IsButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </div>
       </>
     );
   }

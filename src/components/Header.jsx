3fc89fff5ff7f2c4c6c2bd9b4be46bb1b20fb8f0
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-19 11:56:44 -0300

    Task 4 of project done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import LoadingPage from '../pages/LoadingPage';
 
@@ -27,6 +28,11 @@ class Header extends React.Component {
     const { getName, IsLoading } = this.state;
     return (
       <header data-testid="header-component">
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
         { IsLoading ? <LoadingPage /> : (
           <p data-testid="header-user-name">
             Hoi,
